e7095c161c9fee6d0dc9b9ebf233c62912b1bebe
Aleksandr Ishchenko <my.lie.box@gmail.com>
2017-03-09 23:48:10 +0300

    Added types description

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-declare function jarowinkler(a: string | Array<string>, b: string | Array<string>, threshold?: number): number;
-declare function levenshtein(a: string | Array<string>, b: string | Array<string>, options?: {d: number, i: number, s: number}): number;
-declare function ngram(a: string | Array<string>, b: string | Array<string>, ng?: number): number;
-declare function pearson(a: {[key: string]: number}, b: {[key: string]: number}): number;
-declare function jaccard(a: string | Array<string>, b: string | Array<string>): number;
-declare function tanimoto(a: string | Array<string>, b: string | Array<string>): number;
+export declare function jarowinkler(a: string | Array<string>, b: string | Array<string>, threshold?: number): number;
+export declare function levenshtein(a: string | Array<string>, b: string | Array<string>, options?: {d: number, i: number, s: number}): number;
+export declare function ngram(a: string | Array<string>, b: string | Array<string>, ng?: number): number;
+export declare function pearson(a: {[key: string]: number}, b: {[key: string]: number}): number;
+export declare function jaccard(a: string | Array<string>, b: string | Array<string>): number;
+export declare function tanimoto(a: string | Array<string>, b: string | Array<string>): number;
